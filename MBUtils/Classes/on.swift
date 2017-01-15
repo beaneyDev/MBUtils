68ac9292e9f8d8974565a0fb441fc952a50aec70
Matt Beaney <matt.beaney@pagesuite.co.uk>
2017-01-15 18:33:46 +0000

    Added threading functionality

---
+++ MBUtils/Classes/on.swift
@@ -0,0 +1,29 @@
+//
+//  on.swift
+//  Pods
+//
+//  Created by Matt Beaney on 15/01/2017.
+//
+//
+
+import Foundation
+
+class on {
+    static let STANDARD_ANIMATION_TIME = Float(0.3)
+    
+    typealias Work = () -> ()
+    
+    /// Run sth. on the main thread - avoid dispatch overhead if already tehre
+    static func main(_ task: @escaping Work) {
+        if Thread.isMainThread {
+            task()
+        } else {
+            DispatchQueue.main.async(execute: task)
+        }
+    }
+    
+    /// Run sth. on a background thread
+    static func bg(_ task: @escaping Work) {
+        DispatchQueue.global(qos: .default).async(execute: task)
+    }
+}
